dc73624a663c693c17e54a474ea93f4986930435
Yola <jolamajcher12@gmail.com>
2025-12-02 01:44:33 +0100

    feat: expanding flashcards - now, user has the option to either:    * go through the flashcards he marked as "still learning"    * go through the same set once more    * go back to the menu (as he did before)

--- app/src/main/java/com/project/learnasl/flashcards/model/FlashcardsState.kt
+++ app/src/main/java/com/project/learnasl/flashcards/model/FlashcardsState.kt
@@ -16,11 +16,19 @@ data class FlashcardsState (
     var currentIndex by mutableIntStateOf(0)
         private set
 
+    // mutable list of flashcard; either default set of 26 cards or still learning set
+    var currentFlashcards by mutableStateOf(flashcards)
+        private set
+
+    // feature flag; still learning mode on/off
+    var isStillLearningMode by mutableStateOf(false)
+        private set
+
     val currentFlashcard: Flashcard?
-        get() = flashcards.getOrNull(currentIndex)
+        get() = currentFlashcards.getOrNull(currentIndex)
 
     fun moveToNext() {
-        if (currentIndex < flashcards.lastIndex) {
+        if (currentIndex < currentFlashcards.lastIndex) {
             currentIndex++
         }
     }
@@ -37,6 +45,24 @@ data class FlashcardsState (
     fun markAsStillLearning(flashcard: Flashcard){
         if (!stillLearning.contains(flashcard) && !alreadyKnow.contains(flashcard)) {
             stillLearning.add(flashcard)
+        }
+    }
+
+    // repeats the same flashcards set
+    fun resetAll() {
+        currentFlashcards = flashcards
+        currentIndex = 0
+        alreadyKnow.clear()
+        stillLearning.clear()
+        isStillLearningMode = false
+    }
+
+    // creates set made of flashcards marked as "still learning" (swiped left by user)
+    fun createStillLearningSet() {
+        if (stillLearning.isNotEmpty()) {
+            currentFlashcards = stillLearning.toList()
+            currentIndex = 0
+            isStillLearningMode = true
         }
     }
 }
