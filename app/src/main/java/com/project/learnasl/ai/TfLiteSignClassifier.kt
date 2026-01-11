b88a24db9296b23e3cbb5c96449d237289ad0dd1
werixx1 <ciastkouni@gmail.com>
2026-01-11 18:55:46 +0100

    (WIP) custom ai model

--- app/src/main/java/com/project/learnasl/ai/TfLiteSignClassifier.kt
+++ app/src/main/java/com/project/learnasl/ai/TfLiteSignClassifier.kt
@@ -12,7 +12,8 @@ import androidx.core.graphics.get
 
 class TfLiteSignClassifier(
     context: Context,
-    private val threshold: Float = 0.5f //from which score we want to include classification; sureness
+    private val threshold: Float = 0.2f //from which score we want to include classification; sureness
+    // ^im lowering it to 0.2 since i added more classes :) (numbers)
 ) : SignClassifier {
 
     // using interpreter from tensorflow cause the previous one needed normalization that the model did not have :]
@@ -25,12 +26,29 @@ class TfLiteSignClassifier(
         "Delete", "Nothing", "Space"
     )
 
+    // same labels as in training set in custom model
+//    private val labels = listOf(
+//        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f",
+//        "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+//        "w", "x", "y", "z"
+//    )
+
     init {
         val model = FileUtil.loadMappedFile(context, "model.tflite")
+        //val model = FileUtil.loadMappedFile(context, "model_ASL.tflite")
         interpreter = Interpreter(model)
     }
 
     override fun classify(bitmap: Bitmap): List<Classification> {
+
+        // i found this line connected to scaling bitmap *before* using it in classification
+        // maybe that could help with it?? lmk !! if it doesnt work just remove it:)
+        // source - https://firebase.google.com/docs/ml/android/use-custom-models?hl=pl#run_the_interpreter
+        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
+        // ^ (documentation) Creates a new bitmap, scaled from an existing bitmap, when possible. If the specified width and height
+        // are the same as the current width and height of the source bitmap, the source bitmap
+        // is returned and no new bitmap is created
+        // here the same thing - https://medium.com/walmartglobaltech/custom-tensorflow-lite-model-implementation-in-android-5c1c65bd9f97
 
         // TFLite models expect input as a multi dimensional array of floats
         val input = Array(1) { // batch size
@@ -44,7 +62,7 @@ class TfLiteSignClassifier(
         // converting bitmap pixels to normalized floats
         for (y in 0 until 224) {
             for (x in 0 until 224) {
-                val pixel = bitmap[x, y]
+                val pixel = resizedBitmap[x, y] // bitmap[x, y]
                 input[0][y][x][0] = Color.red(pixel) / 255f
                 input[0][y][x][1] = Color.green(pixel) / 255f
                 input[0][y][x][2] = Color.blue(pixel) / 255f
